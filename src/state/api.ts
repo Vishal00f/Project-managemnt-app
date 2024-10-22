95a8c5b79b034fba4a7ea26f57b0f1d4e2e07477
at-vishal <vishal.khandelwal@automatically.cc>
2024-10-22 16:09:46 +0530

    implemented api queries in frontend

--- src/state/api.ts
+++ src/state/api.ts
@@ -19,6 +19,21 @@ export enum Priority {
     Low= "Low",
     Backlog = "Backlog"
 }
+export interface User {
+    id:number;
+    username:string;
+    email:string;
+    profilePictureUrl?:string;
+    cognitoId?:string;
+    teamId?:number
+}
+export interface Attachment{
+    id:number;
+    fileURL:string;
+    fileName:string;
+    taskId:number;
+    uploadedById:number;
+}
 export interface Task {
             id:number
             title:string
@@ -32,12 +47,42 @@ export interface Task {
             projectId:number
             authorUserId?:number
             assignedUserId?:number
+
+            author?:User;
+            assignee?:User;
+            comments?:Comment[];
+            attachments?:Attachment[];
 }
 export const api = createApi({
     baseQuery:fetchBaseQuery({baseUrl:process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath:"api",
-    tagTypes:[],
-    endpoints:(build)=>({})
+    tagTypes:["Projects","Tasks"],
+    endpoints:(build)=>({
+        getProjects:build.query<Project[],void>({
+             query:()=>"projects",
+             providesTags:["Projects"]
+        }),
+        createProject:build.mutation<Project,Partial<Project>>({
+            query:(project)=>({
+                url:"projects",
+                method:"POST",
+                body:project,
+            }),
+            invalidatesTags:["Projects"]
+        }),
+        getTasks:build.query<Task[],{projectId:number}>({
+            query:({projectId})=>`tasks?projectId=${projectId}`,
+            providesTags:(result)=>result?result.map(({id})=>({type:"Tasks" as const, id})):[{type:"Tasks" as const}]
+        }),
+        updateTaskStatus:build.mutation<Task,{taskId:number,status:string}>({
+            query:({taskId,status})=>({
+                url:`tasks/${taskId}/status`,
+                method:"PATCH",
+                body:{status}
+            }),
+            invalidatesTags:(result,error,{taskId})=>[{"type":"Tasks",id:taskId}]
+        })
+    })
 })
 
-export const {} = api;
+export const {useGetProjectsQuery,useCreateProjectMutation,useGetTasksQuery,useUpdateTaskStatusMutation} = api;
